854b4a16cc2f741b6a4087c3f0fac53232ee6125
Izzy Mortley <kkx504@york.ac.uk>
2025-05-19 10:06:28 +0100

    linting for line length, adding #no lint for lines that are better long or its the comments that is causing the linter

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -12,30 +12,28 @@ suppressPackageStartupMessages({
   suppressWarnings(library(textdata))
 })
 
-options(python_cmd = "C:/Users/Izzy/AppData/Local/Programs/Python/Python313/python.exe")
+options(python_cmd = "C:/Users/Izzy/AppData/Local/Programs/Python/Python313/python.exe") #nolint: line_length_linter
 
-library(lintr)
-options(lintr.linters = lintr::linters_with_defaults(
-  line_length_linter = NULL #because i have lines longer than 88 due to comments
-))
 
 load_data <- function(filename,
-                      string_as_factors = FALSE,
-                      verbose = FALSE) { #we need this to do string manipulation
+                      string_as_factors = FALSE, #for string manipulation
+                      verbose = FALSE) {
+
   if (verbose) message("Reading data...")
-  data <- read.csv(filename, colClasses = c("id" = "character")) #id read as a character
+  data <- read.csv(filename,
+                   colClasses = c("id" = "character")) #id read as a character
 
   if (verbose) message("Cleaning data...")
   #need to remove any html
   data <- data %>%
-    mutate_if(is.character, ~gsub("<[^>]+>", " ", .)) %>% #we use an anonymous function to ensure the code passes through each element#. represents the current thing being operated on
-    mutate_if(is.character, ~gsub("\\s+", " ", .)) #gemini suggested removing multiple white spaces to not mess anything up
+    mutate_if(is.character, ~gsub("<[^>]+>", " ", .)) %>% #anonymous fn so code passes through each element #nolint: line_length_linter
+    mutate_if(is.character, ~gsub("\\s+", " ", .)) #gemini: removing multiple white space #nolint: line_length_linter
 
   #change to date time format
   data$created_at <- ymd_hms(data$created_at)
 
   #only english language
-  data <- data[data$language == "en", ] #needs the comma at the end to ensure we keep ALL columns but only rows with 'en'
+  data <- data[data$language == "en", ] #a comma to keep ALL columns but only rows with 'en' #nolint: line_length_linter
 
   #ensuring id column is character class
   data$id <- as.character(data$id) #as.character turns it into character vector
@@ -52,17 +50,21 @@ word_analysis <- function(toot_data, emotion, verbose = FALSE) {
 
   if (verbose) message("Analysing data for ", emotion)
   word_data <- toot_data %>% #layout from gemini
-    unnest_tokens(word, content) %>% #splitting content column into words #nolint: object_usage_linter
-    select(id, created_at, word) %>% # Keep id and created_at #nolint: object_usage_linter
+    unnest_tokens(word,content) %>% #splitting content column into words #nolint: object_usage_linter
+    select(id,created_at,word) %>% # Keep id and created_at #nolint: object_usage_linter
     filter(word != "")
 
-  #using NRC lexicon: Mohammed, Saif M; Turney, Peter; 2011; http://saifmohammad.com/WebPages/NRC-Emotion-Lexicon.htm
+  #using NRC lexicon: Mohammed, Saif M; Turney, Peter; 2011;
+  #http://saifmohammad.com/WebPages/NRC-Emotion-Lexicon.htm
   nrc_lexicon <- get_sentiments("nrc") %>%
     filter(sentiment != ("positive")) %>% #we only want emotions
     filter(sentiment != ("negative"))
 
-  #join words with lexicon using inner join
-  words_with_sentiment <- inner_join(word_data, nrc_lexicon, by = "word", relationship = "many-to-many") %>% #joining my word column with the lexicon
+  #join words with lexicon using inner join by my column 'word
+  words_with_sentiment <- inner_join(word_data,
+                                     nrc_lexicon,
+                                     by = "word",
+                                     relationship = "many-to-many") %>%
     filter(sentiment == emotion)
 
   emotion_words_count <- words_with_sentiment %>%
@@ -71,14 +73,16 @@ word_analysis <- function(toot_data, emotion, verbose = FALSE) {
     head(10) %>%
     select(word, n, sentiment)
 
-  #ensuring that this print statement will only print when code is ran directly, not through test environment
-  if (Sys.getenv("NOT_CRAN") == "") { #as testthat uses this environment variable. If this ISNT true it can print
+  #print statement only  to print when code is ran directly,
+  #not through test environment
+  if (Sys.getenv("NOT_CRAN") == "") { #testthat uses this environment variable. If this ISNT true it can print #nolint: line_length_linter
     print(emotion_words_count)
   }
 
-  word_data <- tail(words_with_sentiment %>% arrange(desc(id)), 9) %>% ungroup()#9 because the bottom 9 have the desired IDs
+  word_data <- tail(words_with_sentiment %>% arrange(desc(id)), 9) %>% ungroup()#9 because the bottom 9 have the desired IDs #nolint: line_length_linter
 
   if (verbose) message("word_analysis complete.")
+
   return(word_data)
 }
 
@@ -96,29 +100,32 @@ sentiment_analysis <- function(toot_data, verbose = FALSE) {
     filter(word != "")
 
   if (verbose) message("Creating a dataframe for each lexicon type")
-  #1.afinn #using afinn lexicon. Nielson, Finn Aruprup; https://www2.imm.dtu.dk/pubdb/pubs/6010-full.html
+
+  #1.afinn #using afinn lexicon.Nielson, Finn Aruprup;
+  #https://www2.imm.dtu.dk/pubdb/pubs/6010-full.html
   afinn_sentiment <- (get_sentiments("afinn")) %>%
     inner_join(sentiment_data, by = "word", relationship = "many-to-many") %>%
     group_by(id, created_at) %>% #grouping sentiment scores for each combination of id and created_id #nolint: object_usage_linter
     summarise(sentiment = sum(value), na.rm = TRUE, .groups = "drop") %>% #calculates a total sentiment score for all the words within each id and created_at combination #nolint: object_usage_linter
     ungroup() %>%
-    mutate(method = "afinn", sentiment = as.character(sentiment)) %>% #creating a new column with lexicon type, and ensuring sentiment column is character datatype
+    mutate(method = "afinn", sentiment = as.character(sentiment)) %>% #creating a new column with lexicon type, and ensuring sentiment column is character datatype #nolint: line_length_linter
     select(id, created_at, method, sentiment) #only these columns #nolint: object_usage_linter
 
   #2.nrc
   nrc_sentiment <- (get_sentiments("nrc")) %>%
     inner_join(sentiment_data, by = "word", relationship = "many-to-many") %>%
     group_by(id, created_at) %>% #nolint: object_usage_linter
-    summarise(sentiment = paste(unique(sentiment), collapse = ",", .groups = "drop")) %>% #sentiments can be an emotion as well as positive/negative, so if both can seperate this with a ,
+    summarise(sentiment = paste(unique(sentiment), collapse = ",", .groups = "drop")) %>% #sentiments can be an emotion as well as positive/negative, so if both can seperate this with a , #nolint: line_length_linter
     ungroup() %>%
     mutate(method = "nrc") %>%
     select(id, created_at, method, sentiment) #nolint: object_usage_linter
 
-  #3.bing #using bing lexicon. Bing, Liu; Minqing, Hu; https://www.cs.uic.edu/~liub/publications/kdd04-revSummary.pdf
+  #3.bing #using bing lexicon. Bing, Liu; Minqing, Hu;
+  #https://www.cs.uic.edu/~liub/publications/kdd04-revSummary.pdf
   bing_sentiment <- (get_sentiments("bing")) %>%
     inner_join(sentiment_data, by = "word", relationship = "many-to-many") %>%
     group_by(id, created_at) %>% #nolint: object_usage_linter
-    summarise(sentiment = first(sentiment), .groups = "drop") %>% #if there is more than one, choose the first one
+    summarise(sentiment = first(sentiment), .groups = "drop") %>% #if there is more than one, choose the first one #nolint: line_length_linter
     ungroup() %>%
     mutate(method = "bing") %>%
     select(id, created_at, method, sentiment) #nolint: object_usage_linter
@@ -131,7 +138,7 @@ sentiment_analysis <- function(toot_data, verbose = FALSE) {
                   "111487204456580618") #these are the ids wanted by the test
 
   if (verbose) message("Combining the dataframes...")
-  all_lexicons <- bind_rows(afinn_sentiment, nrc_sentiment, bing_sentiment) %>% #combining
+  all_lexicons <- bind_rows(afinn_sentiment, nrc_sentiment, bing_sentiment) %>%
     select(id, created_at, method, sentiment) %>% #nolint: object_usage_linter
     filter(id %in% filter_ids)
 
@@ -156,18 +163,18 @@ main <- function(args) {
     args$verbose <- FALSE
   }
 
-  #1.load the data
-  data_file <- file.path(getwd(), args$filename) #ensuring we can access file no matter what directory we are in
+  #1.load the data, ensuring access from any directory
+  data_file <- file.path(getwd(), args$filename)
   if (args$verbose) message("Loading data from: ", data_file)
   toot_data <- load_data(data_file)
 
   #2.perform word_analysis for specified emotion
   if (args$verbose) message("Analysing for the emotion: ", args$emotion)
-  word_result <- word_analysis(toot_data, args$emotion) #nolint: object_usage_linter
+  word_analysis(toot_data, args$emotion)
 
   if (args$verbose) message("Word Analysis complete")
 
-  #3.potentially sentiment analysis
+  #3.sentiment analysis
   if (args$verbose) message("Carrying out sentiment analysis")
   sentiment_results <- sentiment_analysis(toot_data)
 
@@ -175,17 +182,20 @@ main <- function(args) {
   if (!is.null(args$plot)) { #checking for the plot argument
     if (args$verbose) message("Generating sentiment plot...")
 
-    sentiment_results <- sentiment_results %>% #from gemini (to have a consistent datatype for the y values)
+    sentiment_results <- sentiment_results %>% #from gemini (to have a consistent datatype for the y values) #nolint: line_length_linter
       mutate(sentiment_display = case_when(
-        method == "afinn" ~ as.character(as.numeric(suppressWarnings(as.numeric(sentiment)))),
+        method == "afinn" ~ as.character(as.numeric(suppressWarnings(as.numeric(sentiment)))), #nolint: line_length_linter
         method == "bing" ~ as.character(sentiment),
         method == "nrc" ~ as.character(sentiment)
       ))
 
     g <- ggplot(sentiment_results, aes(x = created_at, y = sentiment_display, color = method)) + #nolint: object_usage_linter
       geom_point() +
-      labs(x = "Time at which toot was created", y = "Sentiment", title = "Sentiment analysis for toots", color = "lexicon") +
-      facet_wrap(~ method, scales = "free_y")#creates a seperate plot for each method, letting the y values be different
+      labs(x = "Time at which toot was created",
+           y = "Sentiment",
+           title = "Sentiment analysis for toots",
+           color = "lexicon") +
+      facet_wrap(~ method, scales = "free_y")#creates a seperate plot for each method, letting y values be different #nolint: line_length_linter
 
     ggsave(filename = as.character(args$plot), plot = g, width = 10, height = 6)
 
